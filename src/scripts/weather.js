3c32af5c51d155473828dd3c10e7c945f2f08898
estifanosX <natim6316@gmail.com>
2026-06-23 12:41:06 -0700

    feat:create weather page header

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,3 +1,24 @@
 export function renderWeatherPage() {
-  document.querySelector(".stat-cards").textContent = " weather page is working ";
+  const Container = document.querySelector(".stat-cards")
+
+  Container.innerHTML = `
+<div class="min-h-screen bg-slate-900 text-white> 
+ <div class="mx-auto max-w-7xl p-4 sm:p-6 space-y-6">
+  <!--header content -->
+  <div class="flex flex-col gap-1 border border-slate-800 pb-4" >
+  <h1 class="text-2xl sm:text-3xl font-bold tracking-tight">Weather Environment </h1>
+<p class="text-xs text-slate-400"> Real Time Localized Fetch from open-source</P>
+  </div>
+
+
+
+
+  </div
+ </div>
+  
+  
+  
+  
+  
+  `
 }
