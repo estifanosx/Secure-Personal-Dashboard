6f67727e4d6eeb1dfa7f532d9f1bb56b549cc720
estifanosX <natim6316@gmail.com>
2026-06-24 10:55:57 -0700

    feat:manipulationg stat cards in dashboard page

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,4 +1,13 @@
 export function renderDashboard() {
+  const notes = JSON.parse(localStorage.getItem("notes")) || [];
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const expense = JSON.parse(localStorage.getItem("expense")) || [];
+  const passwords = JSON.parse(localStorage.getItem("password")) || [];
+  let totalExpense = expense.reduce(
+    (accumulator, expense) => accumulator + Number(expense.Amount || 0),
+    0,
+  );
+
   const dashboardPage = document.querySelector(".stat-cards");
   dashboardPage.innerHTML = ` 
   
@@ -13,7 +22,7 @@ export function renderDashboard() {
             >
               <div>
                 <h3 class="text-sm text-slate-400">Notes</h3>
-                <h4 class="text-2xl font-bold">0</h4>
+                <h4 class="text-2xl font-bold">${notes.length}</h4>
                 <span class="text-xs text-slate-500">Total Notes</span>
               </div>
 
@@ -26,7 +35,7 @@ export function renderDashboard() {
             >
               <div>
                 <h3 class="text-sm text-slate-400">Tasks</h3>
-                <h4 class="text-2xl font-bold">0</h4>
+                <h4 class="text-2xl font-bold">${tasks.length}</h4>
                 <span class="text-xs text-slate-500">Pending Tasks</span>
               </div>
 
@@ -39,7 +48,7 @@ export function renderDashboard() {
             >
               <div>
                 <h3 class="text-sm text-slate-400">Expenses</h3>
-                <h4 class="text-2xl font-bold">$0</h4>
+                <h4 class="text-2xl font-bold">${totalExpense}</h4>
                 <span class="text-xs text-slate-500">This Month</span>
               </div>
 
@@ -52,7 +61,7 @@ export function renderDashboard() {
             >
               <div>
                 <h3 class="text-sm text-slate-400">Passwords</h3>
-                <h4 class="text-2xl font-bold">0</h4>
+                <h4 class="text-2xl font-bold">${passwords.length}</h4>
                 <span class="text-xs text-slate-500">Saved Items</span>
               </div>
 
@@ -75,7 +84,7 @@ export function renderDashboard() {
          
       <!-- bottom stat cards -->
 
-      
+
           <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
             <!-- notes stat card  -->
             <div class="p-4 bg-slate-900 border border-slate-800 rounded-xl">
